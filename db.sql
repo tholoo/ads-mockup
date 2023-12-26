5ae1075402d4ec6ed26457534d64417d492fb6a7
Tholo <ali.mohamadza@gmail.com>
2023-12-26 17:09:27 +0330

    enforce positive clicks and views

--- db.sql
+++ db.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS advertiser (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    clicks INT DEFAULT 0,
-    views INT DEFAULT 0
+    clicks INT DEFAULT 0 CHECK (clicks >= 0),
+    views INT DEFAULT 0 CHECK (views >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS ad (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    clicks INT DEFAULT 0,
-    views INT DEFAULT 0,
+    clicks INT DEFAULT 0 CHECK (clicks >= 0),
+    views INT DEFAULT 0 CHECK (views >= 0),
     img_url TEXT,
     link TEXT,
     advertiser_id INT,
