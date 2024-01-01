93d6f932dad970f542c682d1e8fb1ddb79f10eec
Tholo <ali.mohamadza@gmail.com>
2024-01-01 12:48:59 +0330

    use ModelViewSet

--- Yektanet/advertiser_management/views.py
+++ Yektanet/advertiser_management/views.py
@@ -1,42 +1,35 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.generics import RetrieveAPIView, CreateAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Ad, AdClick, Advertiser, AdView
 from .serializers import AdSerializer, AdvertiserSerializer
 
 
-class AdvertiserView(APIView):
-    def get(self, request):
-        advertisers = Advertiser.objects.prefetch_related("ads").all()
+class AdvertiserViewSet(ModelViewSet):
+    queryset = Advertiser.objects.prefetch_related("ads").all()
+    serializer_class = AdvertiserSerializer
 
+    def list(self, request, *args, **kwargs):
         # add a view for each ad
         ip = request.META.get("REMOTE_ADDR")
         AdView.objects.bulk_create([AdView(ad=ad, ip=ip) for ad in Ad.objects.all()])
 
-        serializer = AdvertiserSerializer(advertisers, many=True)
-
-        return Response(serializer.data)
+        return super().list(request, *args, **kwargs)
 
 
-class AdRedirectView(APIView):
-    def get(self, request, pk):
-        ad = get_object_or_404(Ad, pk=pk)
+class AdViewSet(ModelViewSet):
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
 
-        # record a click for the ad
+    # redirect on /ads/<pk>/click/
+    @action(detail=True, methods=["get"], url_path="click")
+    def click(self, request, pk=None):
+        ad = self.get_object()
+
+        # Record a click for the ad
         ip = request.META.get("REMOTE_ADDR")
         AdClick.objects.create(ad=ad, ip=ip)
 
         return Response({"redirect_to": ad.link}, status=status.HTTP_302_FOUND)
-
-
-class AdCreateAPIView(CreateAPIView):
-    queryset = Ad.objects.all()
-    serializer_class = AdSerializer
-
-
-class AdRetrieveAPIView(RetrieveAPIView):
-    queryset = Ad.objects.all()
-    serializer_class = AdSerializer
